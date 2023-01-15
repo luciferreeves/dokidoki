4dccc47a72df89e2990259af326f32da6ee82b90
Bobby <bobbyskhs@gmail.com>
2023-01-15 17:29:11 -0500

    set to single `.expanded` toolbar

--- macos/Runner/MainFlutterWindow.swift
+++ macos/Runner/MainFlutterWindow.swift
@@ -43,17 +43,17 @@ class MainFlutterWindow: NSWindow, NSWindowDelegate {
     self.contentViewController = blurryContainerViewController
     self.setFrame(windowFrame, display: true)
 
-    if #available(macOS 10.13, *) {
-      let customToolbar = NSToolbar()
-      customToolbar.showsBaselineSeparator = false
-      self.toolbar = customToolbar
-    }
+    // if #available(macOS 10.13, *) {
+    //   let customToolbar = NSToolbar()
+    //   customToolbar.showsBaselineSeparator = false
+    //   self.toolbar = customToolbar
+    // }
     self.titleVisibility = .hidden
     self.titlebarAppearsTransparent = true
-    if #available(macOS 11.0, *) {
-      // Use .expanded if the app will have a title bar, else use .unified
-      self.toolbarStyle = .unified
-    }
+    // if #available(macOS 11.0, *) {
+    //   // Use .expanded if the app will have a title bar, else use .unified
+    //   self.toolbarStyle = .unified
+    // }
 
     self.isMovableByWindowBackground = true
     self.styleMask.insert(NSWindow.StyleMask.fullSizeContentView)
@@ -83,20 +83,3 @@ class MainFlutterWindow: NSWindow, NSWindowDelegate {
     hiddenWindowAtLaunch()
   }
 }
-
-// class MainFlutterWindow: NSWindow {
-//   override func awakeFromNib() {
-//     let flutterViewController = FlutterViewController.init()
-//     let windowFrame = self.frame
-//     self.contentViewController = flutterViewController
-//     self.setFrame(windowFrame, display: true)
-
-//     RegisterGeneratedPlugins(registry: flutterViewController)
-
-//     super.awakeFromNib()
-//   }
-//   override public func order(_ place: NSWindow.OrderingMode, relativeTo otherWin: Int) {
-//     super.order(place, relativeTo: otherWin)
-//     hiddenWindowAtLaunch()
-//   }
-// }
